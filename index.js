bea502b30abdd12a0ddc3a5070d84c9a8bd4d754
Tommy Boucher <tommy.boucher@roxwhale.com>
2017-06-09 16:40:48 +0200

    fix react-native 0.45 requirenativecomponent error

--- index.js
+++ index.js
@@ -1,49 +1,45 @@
-import React, {PropTypes} from 'react'
-import ReactNative from 'react-native'
+import React from 'react';
+import PropTypes from 'prop-types';
+import ReactNative from 'react-native';
 
-var {
-	requireNativeComponent,
-	NativeModules,
-	View
-} = ReactNative;
+let { requireNativeComponent, NativeModules, View } = ReactNative;
 
-var RNSpinkit = null;
+let RNSpinkit = null;
 
 class Spinkit extends React.Component {
+  static propTypes = {
+    type: React.PropTypes.string,
+    color: React.PropTypes.string,
+    size: React.PropTypes.number,
+    isVisible: React.PropTypes.bool,
+    testID: React.PropTypes.string,
+    accessibilityComponentType: PropTypes.string,
+    accessibilityLabel: PropTypes.string,
+    accessibilityLiveRegion: PropTypes.string,
+    renderToHardwareTextureAndroid: PropTypes.bool,
+    importantForAccessibility: PropTypes.string,
+    onLayout: PropTypes.func
+  };
 
-	static propTypes = {
-		type: React.PropTypes.string,
-		color: React.PropTypes.string,
-		size: React.PropTypes.number,
-		isVisible: React.PropTypes.bool,
-		testID:React.PropTypes.string,
-		accessibilityComponentType:PropTypes.string,
-		accessibilityLabel:PropTypes.string,
-		accessibilityLiveRegion:PropTypes.string,
-		renderToHardwareTextureAndroid:PropTypes.bool,
-		importantForAccessibility:PropTypes.string,
-		onLayout:PropTypes.func,
-	};
+  static defaultProps = {
+    size: 37,
+    color: '#000000',
+    isVisible: true
+  };
 
-	static defaultProps = {
-		size: 37,
-		color: "#000000",
-		isVisible: true
-	};
+  render() {
+    var size = { height: this.props.size, width: this.props.size };
 
-	render() {
-		var size = {height: this.props.size, width: this.props.size};
-
-		if (!this.props.isVisible) return <View/>;
-		return (
-			<RNSpinkit
-				type={String(this.props.type)}
-				size={parseInt(this.props.size)}
-				color={this.props.color}
-				style={[size, this.props.style]}/>
-		);
-	}
-
+    if (!this.props.isVisible) return <View />;
+    return (
+      <RNSpinkit
+        type={String(this.props.type)}
+        size={parseInt(this.props.size)}
+        color={this.props.color}
+        style={[size, this.props.style]}
+      />
+    );
+  }
 }
 
 // Since RNPM does not recognize `requireNativeComponent`, so we have to
@@ -51,7 +47,10 @@ class Spinkit extends React.Component {
 NativeModules.RNSpinkit;
 
 // Native component
-RNSpinkit = requireNativeComponent('RNSpinkit', Spinkit);
-
+RNSpinkit = requireNativeComponent('RNSpinkit', Spinkit, {
+  nativeOnly: {
+    nativeID: true
+  }
+});
 
 module.exports = Spinkit;
